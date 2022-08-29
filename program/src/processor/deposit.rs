85184d8477e9971fe9b13db8676931a81336ab2f
Onyemaechi Okafor <onyemaechiokafor@Onyemaechis-MBP.lan>
2022-08-29 04:54:02 +0100

    add logs to deposit

--- program/src/processor/deposit.rs
+++ program/src/processor/deposit.rs
@@ -14,6 +14,7 @@ use solana_program::{
     program_error::ProgramError,
     program_pack::{IsInitialized, Pack},
     pubkey::Pubkey,
+    msg
 };
 
 use spl_token::state::Account;
@@ -45,23 +46,28 @@ pub fn init(program_id: &Pubkey, accounts: &[AccountInfo], args: DepositArgs) ->
     }
     assert_owned_by(source_token_info, &spl_token::id())?;
     let source_token: Account = assert_initialized(source_token_info)?;
+    msg!("Assert source token {} is owned by user {}", source_token_info.key, user_info.key);
     assert_token_owned_by(&source_token, user_info.key)?;
     if source_token.mint != *mint_info.key {
         return Err(CardError::InvalidMint.into());
     }
+    msg!("Assertion ok {}", source_token_info.key);
     assert_owned_by(collection_token_info, &spl_token::id())?;
     let collection_token: Account = assert_initialized(collection_token_info)?;
+    msg!("Assert collection token {} is owned by deposit {}", collection_token_info.key, deposit::id());
     assert_token_owned_by(&collection_token, &deposit::id())?;
     if collection_token.mint != *mint_info.key {
         return Err(CardError::InvalidMint.into());
     }
+    msg!("Assertion ok {}", collection_token_info.key);
     assert_owned_by(collection_fee_token_info, &spl_token::id())?;
     let collection_fee_token: Account = assert_initialized(collection_fee_token_info)?;
+    msg!("Assert collection fee token {} is owned by fee {}", collection_fee_token_info.key, fee::id());
     assert_token_owned_by(&collection_fee_token, &fee::id())?;
     if collection_fee_token.mint != *mint_info.key {
         return Err(CardError::InvalidMint.into());
     }
-
+    msg!("Assertion ok {}", collection_fee_token_info.key);
     let fee = calculate_fee(args.amount, args.fee_bps as u64)?;
 
     transfer(false, source_token_info, collection_token_info, user_info, args.amount, &[])?;
